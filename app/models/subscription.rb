a4d6bf19ca455a2eb838cbbdab949b3a996e8ad6
Adreena Fulcher <hurlocene@gmail.com>
2021-07-08 11:18:35 -0400

    migrations done

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,9 @@
 class Subscription < ActiveRecord::Base
-  
+    belongs_to :magazine
+    belongs_to :reader
+
+    def print_details
+        puts "#{self.Reader.name} subscribed to #{self.Magazine.title} for #{self.price}"
+    end
+
 end
